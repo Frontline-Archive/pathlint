9b192f21d2e191a9f4031552cecde95a7e0edbd8
Vincent John Cagay <vjcagay@gmail.com>
2015-08-17 08:54:11 +0800

    Add initial config for unit testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,49 @@
 'use strict';
 
-var gulp         = require( 'gulp' );
-var prettyjson   = require( 'prettyjson' );
-var fileNameLint = require( './lib/filename-lint' );
-
-var config = {
-    // 'nodir' : false,
-    // 'removeFileExt' : false,
-    'globRegexp' : {
-        // 'test/test-files/**' : 'CAMEL_CASE'
-        'test/test-files/**/*.html' : 'DASHED',
-        'test/test-files/**/*.js' :   'CAMEL_CASE',
-    }
-};
+var gulp            = require( 'gulp' );
+var mocha           = require( 'gulp-mocha' );
+var prettyjson      = require( 'prettyjson' );
+var istanbul        = require( 'gulp-istanbul' );
+var fileDirNameLint = require( './lib/filedirname-lint' );
 
 gulp.task( 'test', function () {
-    fileNameLint( config, function ( results ) {
-        console.log( prettyjson.render( results ) );
-    } );
+
+    var paths = {
+        'cover' : [
+            '!lib/**/presets.js',
+            'lib/**/*.js'
+        ],
+        'test' :  [ 'test/(!test-files/)/*.js' ]
+    };
+
+    var mochaOpts = {
+		'ui' :       'bdd',
+		'reporter' : 'spec',
+		'bail' :     true,
+		'globals' :  {
+			'should' : require( 'should' )
+		}
+	};
+
+    return gulp.src( paths.cover )
+        .pipe( istanbul( { 'includeUntested' : true } ) )
+        .pipe( istanbul.hookRequire() )
+        .on( 'finish', function () {
+
+            gulp.src( paths.test, { 'read' : false } )
+                .pipe( mocha( mochaOpts )
+                    .on( 'error', function() {
+                        process.exit( 1 );
+                    } ) )
+                .pipe( istanbul.writeReports( {
+                    'reporters' : [ 'text', 'text-summary' ]
+                } ) )
+                .on( 'error', function () {
+                    process.exit( 1 );
+                } )
+                .on( 'end', function () {
+                    process.exit( 1 );
+                } );
+
+        } );
 } );
